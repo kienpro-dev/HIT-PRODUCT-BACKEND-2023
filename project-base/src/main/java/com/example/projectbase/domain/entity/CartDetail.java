d77b82c7001b8a7278277ef65f9f5c16a2e246aa
= <trieudangtuan18032003@gmail.com>
2023-08-04 21:18:00 +0700

    Add shop_product_detail entity, fix api find product

--- project-base/src/main/java/com/example/projectbase/domain/entity/CartDetail.java
+++ project-base/src/main/java/com/example/projectbase/domain/entity/CartDetail.java
@@ -5,6 +5,7 @@ import lombok.*;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.List;
 
 
 @Entity
@@ -16,18 +17,21 @@ import java.io.Serializable;
 public class CartDetail implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "cart_detail_id",insertable = false, updatable = false, nullable = false)
+    @Column(name = "cart_detail_id", insertable = false, updatable = false, nullable = false)
     private int id;
 
     @ManyToOne(cascade = CascadeType.MERGE)
-    @JoinColumn(name = "product_id",foreignKey = @ForeignKey(name = "FK_CART_PRODUCT1"),referencedColumnName = "product_id")
+    @JoinColumn(name = "product_id", foreignKey = @ForeignKey(name = "FK_CART_PRODUCT1"), referencedColumnName = "product_id")
     private Product product;
 
 
     @ManyToOne(cascade = CascadeType.MERGE)
-    @JoinColumn(name = "cart_id",foreignKey = @ForeignKey(name = "FK_CART_PRODUCT2"),referencedColumnName = "cart_id")
+    @JoinColumn(name = "cart_id", foreignKey = @ForeignKey(name = "FK_CART_PRODUCT2"), referencedColumnName = "cart_id")
     private Cart cart;
 
     @Column(nullable = false)
     private int quantity;
+
+    @OneToMany(cascade = CascadeType.MERGE, mappedBy = "cartDetail")
+    List<ShopProductDetail> shopProductDetail;
 }
