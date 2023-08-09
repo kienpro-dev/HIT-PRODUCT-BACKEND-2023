645a3768685716a6c80e6c1dd0d0c8d5f0071f3a
kienpro-dev <tienkiennropro@gmail.com>
2023-08-09 12:54:12 +0700

    Fix pageable bill

--- project-base/src/main/java/com/example/projectbase/controller/BillController.java
+++ project-base/src/main/java/com/example/projectbase/controller/BillController.java
@@ -3,14 +3,18 @@ package com.example.projectbase.controller;
 import com.example.projectbase.base.RestApiV1;
 import com.example.projectbase.base.VsResponseUtil;
 import com.example.projectbase.constant.UrlConstant;
+import com.example.projectbase.domain.dto.pagination.PaginationFullRequestDto;
 import com.example.projectbase.service.BillDetailService;
 import com.example.projectbase.service.BillService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.api.annotations.ParameterObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+
+import javax.validation.Valid;
 
 
 @RequiredArgsConstructor
@@ -21,8 +25,8 @@ public class BillController {
 
     @Operation(summary = "API get all bill")
     @GetMapping(UrlConstant.Bill.BILLS)
-    public ResponseEntity<?> getAllBill() {
-        return VsResponseUtil.success(billDetailService.getAllBill());
+    public ResponseEntity<?> getBills(@Valid @ParameterObject PaginationFullRequestDto requestDTO) {
+        return VsResponseUtil.success(billDetailService.getAllBill(requestDTO));
     }
 
     @Operation(summary = "API cancel order")
