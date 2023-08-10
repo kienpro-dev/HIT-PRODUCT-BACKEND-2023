f28e7cdd927e44bfcf0b6c14b8370dec9161051f
= <trieudangtuan18032003@gmail.com>
2023-08-10 23:20:30 +0700

    Fix page_size_default

--- project-base/src/main/java/com/example/projectbase/constant/CommonConstant.java
+++ project-base/src/main/java/com/example/projectbase/constant/CommonConstant.java
@@ -4,7 +4,7 @@ public class CommonConstant {
 
   public static final String SORT_TYPE_ASC = "ASC";
   public static final String SORT_TYPE_DESC = "DESC";
-  public static final Integer PAGE_SIZE_DEFAULT = 10;
+  public static final Integer PAGE_SIZE_DEFAULT = 12;
 
   public static final Integer ZERO_INT_VALUE = 0;
   public static final Integer ONE_INT_VALUE = 1;
